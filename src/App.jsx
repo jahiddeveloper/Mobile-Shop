52743596cbff26a4a9f828724c0a469f8ac8ae02
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2026-01-09 18:51:14 +0600

    btn toggleing compleat

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import "./App.css";
 import AvailableMobile from "./Componenet/AvailableMobile/AvailableMobile";
 import Hero from "./Componenet/Hero/Hero";
@@ -9,10 +9,34 @@ import Footer from "./Componenet/Footer/Footer";
 let fetchMobile = fetch("/Mobile.json").then((res) => res.json());
 
 function App() {
+  let [toggle, setToggle] = useState(true);
+
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+
+      <div className="container mx-auto mt-10 flex justify-between items-center">
+        <h2 className="font-bold text-2xl">
+          {toggle === true ? "Available Mobile" : "Selected Mobile"}
+        </h2>
+
+        <div>
+          <button
+            onClick={() => setToggle(true)}
+            className={`px-5 py-3 ${toggle === true ? "bg-red-500" : ""} ${toggle === true ? "text-white" : ""} ${toggle === true ? "font-bold" : ""} rounded-l-xl cursor-pointer`}
+          >
+            Availabale
+          </button>
+          <button
+            onClick={() => setToggle(false)}
+            className={`px-5 py-3 ${toggle === false ? "bg-red-500" : ""} ${toggle === false ? "text-white" : ""} ${toggle === false ? "font-bold" : ""} rounded-r-xl cursor-pointer`}
+          >
+            Selected <span>(0)</span>
+          </button>
+        </div>
+      </div>
+
       <Suspense fallback={<h3>Just a sec...</h3>}>
         <AvailableMobile fetchMobile={fetchMobile}></AvailableMobile>
       </Suspense>
